e6630c220f33c28bc2f56d3d30dda118fb180d53
Sabitha Paulraj <118343379+sabithapaulraj@users.noreply.github.com>
2025-08-09 20:53:57 +0530

    Delete basicsearch directory

--- basicsearch/src/main/java/com/searchapp/basicsearch/dto/SearchRequest.java
+++
@@ -1,9 +0,0 @@
-package com.searchapp.basicsearch.dto;
-
-import lombok.Data;
-
-@Data
-public class SearchRequest {
-    private String interest;
-    private int minSalary;
-}
--- basicsearch/src/main/java/com/searchapp/basicsearch/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.searchapp.basicsearch.repository;
-
-import com.searchapp.basicsearch.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
